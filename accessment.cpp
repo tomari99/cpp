e48aebd7ca9cd87ec8cb93506a767ca9c6d3817b
tomari99 <108823930+tomari99@users.noreply.github.com>
2022-09-26 13:50:59 +0630

    Update accessment.cpp

--- accessment.cpp
+++ accessment.cpp
@@ -32,21 +32,59 @@ class User{
 };
 
 int main(){
-    User user1;
-    cout << "Enter User ID : ";
-    cin >> user1.id;
-    cout << "Enter User Name : ";
-    cin >> user1.userName;
-    cout << "Enter User age : ";
-    cin >> user1.age;
-    cout << "Enter amount : ";
-    cin >> user1.amount;
-    cout << "Enter Location : ";
-    cin >> user1.location;
-    cout << "Enter Password : ";
-    cin >> user1.password;
-    user1.setData(user1.id ,user1.userName ,user1.age ,user1.amount ,user1.location ,user1.password);
-    user1.getData();
+    int insert = 0;
+    int uinput = 0;
+    cout <<"Press 1 to insert data \nPress 2 to read data \n";
+    cout << "\nYour input : ";
+
+    cin >>uinput;
+    User user[10];
+    fstream userData;
+    if(uinput == 1){
+    cout << "How many user u want to insert : ";
+    cin >> insert;
+
+        for(int i = 0;i<insert;i++){
+        cout << "Enter User ID : ";
+        cin >> user[i].id;
+        cout << "Enter User Name : ";
+        cin >> user[i].userName;
+        cout << "Enter User age : ";
+        cin >> user[i].age;
+        cout << "Enter amount : ";
+        cin >> user[i].amount;
+        cout << "Enter Location : ";
+        cin >> user[i].location;
+        cout << "Enter Password : ";
+        cin >> user[i].password;
+        user[i].setData(user[i].id ,user[i].userName ,user[i].age ,user[i].amount ,user[i].location ,user[i].password);
+
+        userData.open("User.txt",ios::app);
+        if(userData.is_open()){
+            userData <<"\nID :"<<user[i].id<<"\nUsername :"<<user[i].userName<<"\nAge :"<<user[i].age<<"\nAmount :"<<user[i].amount<<"\nLocation :"<<user[i].location<<"\nPassword :"<<user[i].password;
+            userData.close();
+        }
+        }
+    }
+    if(uinput == 2){
+
+    userData.open("user.txt",ios::in);
+        if(!userData.is_open()){
+            cout << "There is no data";
+            userData.close();
+        }
+
+        if(userData.is_open()){
+
+            string line;
+            while(getline(userData,line)){
+
+                cout << line <<endl;
+            }
+
+            userData.close();
+        }
+    }
 
 return 0;
 }
